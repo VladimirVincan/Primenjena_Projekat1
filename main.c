ff35a3e51da5941f099f785475cdb8310d99a7f4
ZekiDj <rasevicnikola@yahoo.com>
2019-01-07 21:08:23 +0100

    zeki test

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
-int main(){
-    return 0;
+/* 
+ * File:   main.c
+ * Author: ZekiDj
+ *
+ * Created on January 7, 2019, 9:07 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
 }
+

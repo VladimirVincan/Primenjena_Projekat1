f09b41289fd40a4ea2bb7418231b77e4dccd2563
VladimirVincan <VladimirVincan@gmail.com>
2019-01-07 20:40:16 +0100

    test

---
+++ newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: Vladimir Vincan
+ *
+ * Created on January 7, 2019, 8:39 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
